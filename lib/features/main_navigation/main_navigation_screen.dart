cfe4e816132d589be3ddfbc6d55a620681034d6d
Heochan <heochan01@korea.ac.kr>
2025-01-30 18:55:17 +0900

    Feat: 내비게이션 바들 중 BottomNavigationBar 공부 (material design v2 위젯)

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final screens = [
+    Center(
+      child: Text('Home'),
+    ),
+    Center(
+      child: Text("Search"),
+    )
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // selectedItemColor: Theme.of(context).primaryColor,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What are you?",
+            backgroundColor: Colors.teal,
+          ),
+        ],
+      ),
+    );
   }
 }
